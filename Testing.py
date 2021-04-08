9ea4f5bb428d2d9faf5b49bd4de7db4cd9dae85b
arturo <anegreiross@outlook.com>
2021-04-08 18:35:05 -0500

    ok

--- Testing.py
+++ Testing.py
@@ -4,8 +4,26 @@ import numpy as np
 import pandas as pd
 
 PATH = "practices/Salaries.csv"
-data = pd.read_csv(PATH)
 
-for i in data:
-    print(i)
 
+data = pd.read_csv(PATH,low_memory=False)
+#print(data.info())
+
+
+# Calculating the base pay
+
+
+print(data.head(2))
+
+data.dropna(inplace= True)
+print(data.info())
+
+print(data['BasePay'])
+print(data['BasePay'].mean())
+
+"""
+print("\n")
+#print(data['BasePay'].std())
+print("\n")
+#print(data['BasePay'].sum())
+"""
