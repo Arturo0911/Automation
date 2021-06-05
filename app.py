3186b54df52328c7c1a683211e7d4928320fb5ec
Arturo Negreiros <anegreiross@outlook.com>
2021-06-04 22:34:21 -0500

    udpated Pipefile

---
+++ app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def create_environment(variable=None, index=None) -> int:
+    if variable is None and index is None:
+        return 0
+    else:
+        return ""
+
+
+def main():
+    print(create_environment())
+
+
+main()
